6f0d076644d3fa24a17604835fb7191321d0a6d7
AlexStarc <sandrstar@yandex.ru>
2011-03-01 21:08:19 +0300

    AlexStarc initial project commit

---
+++ MM5/src/mm5/gallery/gallery.java
@@ -0,0 +1,13 @@
+package mm5.gallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class gallery extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
