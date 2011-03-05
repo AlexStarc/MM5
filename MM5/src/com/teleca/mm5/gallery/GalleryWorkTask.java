3321ec8cb1b86fb8bde58553175d5c88ebf1cf53
AlexStarc <sandrstar@yandex.ru>
2011-03-05 12:42:53 +0300

    initial draft of content preparing for thumbnails/list

--- MM5/src/com/teleca/mm5/gallery/GalleryWorkTask.java
+++ MM5/src/com/teleca/mm5/gallery/GalleryWorkTask.java
@@ -1,184 +1,167 @@
 package com.teleca.mm5.gallery;
 
-import android.os.AsyncTask;
-import android.os.Environment;
-import android.util.Log;
-import android.provider.MediaStore;
-import android.net.Uri;
-import android.database.Cursor;
-import java.lang.IllegalArgumentException;
+import java.io.FileInputStream;
+
 import android.content.ContentResolver;
 import android.content.Intent;
-
-import java.util.ArrayList;
-
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 
 //enum for results of work task processing
 enum GalleryWorkTaskResult {
-	GALLERY_RESULT_EMPTY,
-	GALLERY_RESULT_FINISHED,
-	GALLERY_RESULT_ERROR,
+    GALLERY_RESULT_EMPTY,
+    GALLERY_RESULT_FINISHED,
+    GALLERY_RESULT_ERROR,
 }
 
 enum GalleryWorkTaskContentType {
-	GALLERY_AUDIO,
-	GALLERY_IMAGES
+    GALLERY_AUDIO,
+    GALLERY_IMAGES
 }
 
 @ClassPreamble (
-		author = "Alexander Starchenko",
-		description = "applet work task, main purpose - interact with fs and providers"
-	)
+                author = "Alexander Starchenko",
+
+                description = "applet work task, main purpose - interact with fs and providers"
+)
 class GalleryWorkTask extends AsyncTask<GalleryWorkTaskContentType, Integer, GalleryWorkTaskResult> {
-	private static final String TAG = "GalleryWorkTask";
-	private GalleryView parentGalleryView;
-	ContentResolver mainContentResolver;
-	Cursor mainContentCursor;
-	ArrayList<GalleryContentItem> resultingContentList;
-	
-	/** Constructors of async gallery work task */
-	public GalleryWorkTask() {
-	}	
-	
-	public GalleryWorkTask(GalleryView parentGalleryView) {
-		this.setParentGalleryView(parentGalleryView);
-	}
-	
-	/** below function contents get executed in a separate thread */
-	protected GalleryWorkTaskResult doInBackground(GalleryWorkTaskContentType... type) {
-		GalleryWorkTaskResult resultProcessing;
-		String mediaStorageState = Environment.getExternalStorageState();
-		
-		Log.i( TAG, "work task bg processing started with media state " + mediaStorageState);
-		
-		if (Environment.MEDIA_MOUNTED.equals(mediaStorageState)) {
-			// first of all ensure we'll get refreshed data by sending intent to MediaScanner
-			try {
-				parentGalleryView.getGalleryViewContext().sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://"+ Environment.getExternalStorageDirectory())));
-			}
-			catch(Exception e) {
-				Log.e( TAG, "something wrong with sendBroadcast: " + e.getClass() + " thrown " + e.getMessage());
-			}
+    private static final String TAG = "GalleryWorkTask";
+    private GalleryView parentGalleryView;
+    ContentResolver mainContentResolver;
+    Cursor mainContentCursor;
+    GalleryContentItem[] resultingContentList;
 
-			// now wait until all contents are scanned
-			try {
-				while( isMediaScannerScanning() );
-			}
-			catch(Exception e) {
-				Log.e( TAG, "something wrong with the waiting: " + e.getClass() + " thrown " + e.getMessage());
-			}
-			
-			switch(type[0]) {
-			case GALLERY_AUDIO:
-				break;
-			
-			case GALLERY_IMAGES:
-				mainContentCursor = MediaStore.Images.Media.query(mainContentResolver,
-																  MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-														          null );
-				break;
-				
-			default:
-				Log.i( TAG, "incorrect parameter encounted" );
-				throw new IllegalArgumentException("incorrect GalleryWorkTaskContentType passed by view");
-			}
-		
-			// here we get URI and ready to prepare query, however, need to cancel previous one
-			if( null != mainContentResolver && null == mainContentCursor ) {
-				mainContentCursor = mainContentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, null);			
-			}
-			
-			if( null != mainContentCursor ) {
-				String name;
-				String assetData;
-				Boolean recordsAvailable = false;
-				int nameColumn = 0;
-				int dataColumn = 0;
-				
-				try {
-					recordsAvailable = mainContentCursor.moveToFirst();
-					
-					if( recordsAvailable  ) {
-						nameColumn = mainContentCursor.getCount();
-						nameColumn = mainContentCursor.getColumnIndex(type[0] == GalleryWorkTaskContentType.GALLERY_AUDIO ?
-																	  MediaStore.Audio.Media.DISPLAY_NAME :
-																	  MediaStore.Images.Media.DISPLAY_NAME);
-						dataColumn = mainContentCursor.getColumnIndex(type[0] == GalleryWorkTaskContentType.GALLERY_AUDIO ?
-																	  MediaStore.Audio.Media.DATA :
-																	  MediaStore.Images.Media.DATA);
-					
-						do {
-							name = mainContentCursor.getString(nameColumn);
-							assetData = mainContentCursor.getString(dataColumn);
-							Log.i(TAG, "content display name " + name);
-						} while( mainContentCursor.moveToNext() );
-					} else {
-						Log.e( TAG, "No records available!");
-					}
-				}
-				catch(Exception e) {
-					Log.e( TAG, "something wrong with the base: " + e.getClass() + " thrown " + e.getMessage());
-				}
-			}
-		}
-		// set processing end result
-		resultProcessing = GalleryWorkTaskResult.GALLERY_RESULT_FINISHED;
-		return resultProcessing;
-	}
-	
-	protected void onProgressUpdate(Integer... progress) {
-		parentGalleryView.progressWorkExecution(1);
-	}
-	
-	protected void onPostExecute(GalleryWorkTaskResult result) {
-		Log.i( TAG, "bg processign finished, invoke back parent view" );
-		/* Share execution resulted list with parent view,
-		 * where's no need to share it if its empty */
-		if( null != resultingContentList &&
-			!resultingContentList.isEmpty() ) {
-			parentGalleryView.setContentList(resultingContentList);
-		}
+    /** Constructors of async gallery work task */
+    public GalleryWorkTask() {
+    }
 
-		/* Share status with parent view in order to notify about errors, if any */
-		parentGalleryView.finishedWorkExecution(result);
-	}
+    public GalleryWorkTask(GalleryView parentGalleryView) {
+        this.setParentGalleryView(parentGalleryView);
+    }
 
-	/**
-	 * @param parentGalleryView the parentGalleryView to set
-	 */
-	protected void setParentGalleryView(GalleryView parentGalleryView) {
-		this.parentGalleryView = parentGalleryView;
-		
-		// get content provider here also
-		if( null != parentGalleryView ) {
-			mainContentResolver = parentGalleryView.getGalleryViewContext().getContentResolver();
-		}
-	}
+    /** below function contents get executed in a separate thread */
+    @Override
+    protected GalleryWorkTaskResult doInBackground(GalleryWorkTaskContentType... type) {
+        GalleryWorkTaskResult resultProcessing;
+        String mediaStorageState = Environment.getExternalStorageState();
 
-	/**
-	 * @return the parentGalleryView
-	 */
-	protected GalleryView getParentGalleryView() {
-		return parentGalleryView;
-	}
-	
-	
-    private boolean isMediaScannerScanning() {
-        boolean result = false;
-        Uri scannerUri = MediaStore.getMediaScannerUri();
-        Cursor cursor = parentGalleryView.getGalleryViewContext().getContentResolver().query( scannerUri, 
-        					   																  null,
-        					   																  null,
-        					   																  null,
-        					   																  null );
-        if (cursor != null) {
-            if (cursor.getCount() == 1) {
-                cursor.moveToFirst();
-                result = "external".equals(cursor.getString(0));
+        Log.i( TAG, "work task bg processing started with media state " + mediaStorageState);
+
+        if (Environment.MEDIA_MOUNTED.equals(mediaStorageState)) {
+            // first of all ensure we'll get refreshed data by sending intent to MediaScanner
+            try {
+                parentGalleryView.getGalleryViewContext().sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://"+ Environment.getExternalStorageDirectory())));
             }
-            cursor.close(); 
-        } 
+            catch(Exception e) {
+                Log.e( TAG, "something wrong with sendBroadcast: " + e.getClass() + " thrown " + e.getMessage());
+            }
 
-        return result;
+            switch(type[0]) {
+            case GALLERY_AUDIO:
+                break;
+
+            case GALLERY_IMAGES:
+                mainContentCursor = MediaStore.Images.Media.query(mainContentResolver,
+                                                                  MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                                                  null );
+
+                break;
+
+            default:
+                Log.i( TAG, "incorrect parameter encounted" );
+                throw new IllegalArgumentException("incorrect GalleryWorkTaskContentType passed by view");
+            }
+
+            // here we get URI and ready to prepare query, however, need to cancel previous one
+            if( null != mainContentResolver && null == mainContentCursor ) {
+                mainContentCursor = mainContentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
+            }
+
+            if( null != mainContentCursor ) {
+                String name;
+                String assetData;
+                Boolean recordsAvailable = false;
+                int nameColumn = 0;
+                int dataColumn = 0;
+                int contentCount = 0;
+                int i = 0;
+
+                try {
+                    recordsAvailable = mainContentCursor.moveToFirst();
+                    contentCount = mainContentCursor.getCount();
+
+                    if( recordsAvailable && 0 <= contentCount ) {
+                        nameColumn = mainContentCursor.getColumnIndex(type[0] == GalleryWorkTaskContentType.GALLERY_AUDIO ?
+                                MediaStore.Audio.Media.DISPLAY_NAME :
+                                    MediaStore.Images.Media.DISPLAY_NAME);
+                        dataColumn = mainContentCursor.getColumnIndex(type[0] == GalleryWorkTaskContentType.GALLERY_AUDIO ?
+                                MediaStore.Audio.Media.DATA :
+                                    MediaStore.Images.Media.DATA);
+
+                        resultingContentList = new GalleryContentItem[contentCount];
+
+                        do {
+                            name = mainContentCursor.getString(nameColumn);
+                            assetData = mainContentCursor.getString(dataColumn);
+                            Log.i(TAG, "content display name " + name);
+
+                            // Here we're assuming that DATA stores PATH and try it queue
+                            resultingContentList[i] = new GalleryContentItem( BitmapFactory.decodeStream( new FileInputStream( assetData ) ),
+                                                                              name );
+                        } while( mainContentCursor.moveToNext() && i < contentCount );
+                    } else {
+                        Log.e( TAG, "No records available!");
+                    }
+                }
+                catch(Exception e) {
+                    Log.e( TAG, "something wrong with the base: " + e.getClass() + " thrown " + e.getMessage());
+                }
+            }
+        }
+        // set processing end result
+        resultProcessing = GalleryWorkTaskResult.GALLERY_RESULT_FINISHED;
+        return resultProcessing;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... progress) {
+        parentGalleryView.progressWorkExecution(1);
+    }
+
+    @Override
+    protected void onPostExecute(GalleryWorkTaskResult result) {
+        Log.i( TAG, "bg processign finished, invoke back parent view" );
+        /* Share execution resulted list with parent view,
+         * where's no need to share it if its empty */
+        if( null != resultingContentList && 0 != resultingContentList.length ) {
+            parentGalleryView.setContentList(resultingContentList);
+        }
+
+        /* Share status with parent view in order to notify about errors, if any */
+        parentGalleryView.finishedWorkExecution(result);
+    }
+
+    /**
+     * @param parentGalleryView the parentGalleryView to set
+     */
+    protected void setParentGalleryView(GalleryView parentGalleryView) {
+        this.parentGalleryView = parentGalleryView;
+
+        // get content provider here also
+        if( null != parentGalleryView ) {
+            mainContentResolver = parentGalleryView.getGalleryViewContext().getContentResolver();
+        }
+    }
+
+    /**
+     * @return the parentGalleryView
+     */
+    protected GalleryView getParentGalleryView() {
+        return parentGalleryView;
     }
 }
--- MM5/src/com/teleca/mm5/gallery/gallery.java
+++ MM5/src/com/teleca/mm5/gallery/gallery.java
@@ -1,74 +1,77 @@
 package com.teleca.mm5.gallery;
 
-import java.lang.annotation.*;
-import java.util.ArrayList;
+import java.lang.annotation.Documented;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.content.Context;
 
 @Documented
 @interface ClassPreamble {
-	   String author();
-	   int currentRevision() default 1;
-	   String description();
-	}
+    String author();
+    int currentRevision() default 1;
+    String description();
+}
 
 // enum for available gallery views to be used in methods
 enum GalleryViewTypes {
-	GALLERY_LIST,
-	GALLERY_THUMBNAILS,
-	GALLERY_FULLSCREEN,
-	GALLERY_DETAILS
+    GALLERY_LIST,
+    GALLERY_THUMBNAILS,
+    GALLERY_FULLSCREEN,
+    GALLERY_DETAILS
 }
 
 @ClassPreamble (
-		author = "Alexander Starchenko",
-		description = "interface for view for interaction with GalleryWorkTask task"
-	)
+                author = "Alexander Starchenko",
+                description = "interface for view for interaction with GalleryWorkTask task"
+)
 interface GalleryView {
-	public void progressWorkExecution( int NumberFiles );
-	public void finishedWorkExecution( GalleryWorkTaskResult processingResult );
-	public void setContentList( ArrayList<GalleryContentItem> contentList );
-	public Context getGalleryViewContext();
+    public void progressWorkExecution( int NumberFiles );
+    public void finishedWorkExecution( GalleryWorkTaskResult processingResult );
+    public void setContentList( GalleryContentItem[] contentArray );
+    public Context getGalleryViewContext();
 }
 
 @ClassPreamble (
-		author = "Alexander Starchenko",
-		description = "main applet class"
-	)
+                author = "Alexander Starchenko",
+                description = "main applet class"
+)
 public class gallery extends Activity implements GalleryView {
-	private static final String TAG = "gallery";
-	
+    private static final String TAG = "gallery";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	GalleryWorkTask galleryWorkBg = new GalleryWorkTask(this); 
+        GalleryWorkTask galleryWorkBg = new GalleryWorkTask(this);
 
-    	Log.i( TAG, "Started" );
+        Log.i( TAG, "Started" );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         /* start background work process execution */
-        galleryWorkBg.execute(GalleryWorkTaskContentType.GALLERY_AUDIO);
+        galleryWorkBg.execute(GalleryWorkTaskContentType.GALLERY_IMAGES);
     }
 
+    @Override
     public void progressWorkExecution( int NumberFiles ) {
-    	Log.i( TAG, "BG work has" + NumberFiles );
+        Log.i( TAG, "BG work has" + NumberFiles );
     }
-    
+
+    @Override
     public void finishedWorkExecution( GalleryWorkTaskResult processingResult ) {
-    	Log.i( TAG, "BG work has finished with " + processingResult );
+        Log.i( TAG, "BG work has finished with " + processingResult );
     }
-    
-    public void setContentList( ArrayList<GalleryContentItem> contentList ) {
-    	/** Here all items received by view via ArrayList. It get called just before finishedWorkExecution;
-    	 *  Contents of view might needs to be updated only after status received via finishedWorkExecution */
-    	// TODO: provide list updating
+
+    @Override
+    public void setContentList( GalleryContentItem[] contentArray ) {
+        /** Here all items received by view via ArrayList. It get called just before finishedWorkExecution;
+         *  Contents of view might needs to be updated only after status received via finishedWorkExecution */
+        // TODO: provide list updating
     }
-    
+
+    @Override
     public Context getGalleryViewContext() {
-    	return getApplicationContext();
+        return getApplicationContext();
     }
 }
